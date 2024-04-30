5243917e4bfe3c195a3174e3c29fa49d2f28ce72
Rafia Anzum <dlagrandy@gmail.com>
2024-04-30 10:50:37 +0600

    put input display in styled paragraph and add spaces around operations and made other changes accordingly

--- javascript.js
+++ javascript.js
@@ -2,34 +2,38 @@ let resultContainer = document.querySelector(".result-container");
 let functionContainer = document.querySelector(".function-container");
 let buttons = document.querySelectorAll("button");
 
+let historyPara = document.querySelector(".history-para");
+let inputPara = document.querySelector(".input-para");
+
 let operations = ["+", "-", "×", "÷", "a"]
 
 buttons.forEach(button => {
     button.addEventListener("click", event => {
-        let lastBtn = resultContainer.textContent.at(-1);
+        let lastBtn = inputPara.textContent.at(-1);
         let currentBtn = button.getAttribute("id").at(-1);
 
         if (currentBtn === "r") { /*refers to clear*/
-            resultContainer.textContent = "";
+            inputPara.textContent = "";
 
         } else if (currentBtn === "e") { /*refers to delete*/
-            resultContainer.textContent = resultContainer.textContent.slice(0, -1)
+            if (lastBtn === " ") inputPara.textContent = inputPara.textContent.slice(0, -3)
+            else inputPara.textContent = inputPara.textContent.slice(0, -1)
 
         } else if (operations.includes(currentBtn)) {
-            if (lastBtn === "." || !isNaN(lastBtn)) {
-                resultContainer.textContent += currentBtn;
+            if (lastBtn === "." || !isNaN(lastBtn) && lastBtn !== " ") {
+                inputPara.textContent += ` ${currentBtn} `;
             }
 
         } else if (currentBtn === ".") {
-            if (!isNaN(lastBtn)) {
-                resultContainer.textContent += currentBtn;
+            if (!isNaN(lastBtn) && lastBtn !== " ") {
+                inputPara.textContent += currentBtn;
             }
 
         } else {
-            if (lastBtn === "0" && isNaN(resultContainer.textContent.at(-2))) {
-                resultContainer.textContent = resultContainer.textContent.slice(0, -1) + currentBtn;
+            if (lastBtn === "0" && (inputPara.textContent.at(-2) === " " || !inputPara.textContent.at(-2))) {
+                inputPara.textContent = inputPara.textContent.slice(0, -1) + currentBtn;
             } else {
-                resultContainer.textContent += currentBtn;
+                inputPara.textContent += currentBtn;
             }
         }
     })
