550e4fbdf22226d5d381d297cb13462134a0f623
Rafia Anzum <dlagrandy@gmail.com>
2024-05-01 20:15:59 +0600

    add the numbers and operators in a string

--- javascript.js
+++ javascript.js
@@ -7,6 +7,8 @@ let inputPara = document.querySelector(".input-para");
 
 let operations = ["+", "-", "×", "÷", "a"]
 
+let calculationString = "";
+
 buttons.forEach(button => {
     button.addEventListener("click", event => {
         let lastBtn = inputPara.textContent.at(-1);
@@ -14,27 +16,36 @@ buttons.forEach(button => {
 
         if (currentBtn === "r") { /*refers to clear*/
             inputPara.textContent = "";
+            calculationString = "";
 
         } else if (currentBtn === "e") { /*refers to delete*/
             if (lastBtn === " ") inputPara.textContent = inputPara.textContent.slice(0, -3)
             else inputPara.textContent = inputPara.textContent.slice(0, -1)
+            calculationString = calculationString.slice(0, -1);
 
         } else if (operations.includes(currentBtn)) {
             if (lastBtn === "." || !isNaN(lastBtn) && lastBtn !== " ") {
                 inputPara.textContent += ` ${currentBtn} `;
+                if (currentBtn === "×") calculationString += "*";
+                else if (currentBtn === "÷") calculationString += "/";
+                else calculationString += currentBtn;
             }
 
         } else if (currentBtn === ".") {
             if (!isNaN(lastBtn) && lastBtn !== " ") {
                 inputPara.textContent += currentBtn;
+                calculationString += currentBtn;
             }
 
         } else {
             if (lastBtn === "0" && (inputPara.textContent.at(-2) === " " || !inputPara.textContent.at(-2))) {
                 inputPara.textContent = inputPara.textContent.slice(0, -1) + currentBtn;
+                calculationString = calculationString.slice(0, -1) + currentBtn;
             } else {
                 inputPara.textContent += currentBtn;
+                calculationString += currentBtn;
             }
         }
+        console.log(calculationString)
     })
 })
